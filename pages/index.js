c94587a9fbe3c602137178e1be924783899a1dcc
AdrianTejada <atejada@my.bcit.ca>
2022-02-15 19:24:33 -0800

    merged with gurvir

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import CardPlaceHolder from "@/Comps/CardPlaceHolder";
 
 export default function Home() {
@@ -7,24 +6,5 @@ export default function Home() {
     <img src={`https://img.pokemondb.net/sprites/black-white/normal/${name}.png`} alt="Ivysaur"/>
     <CardPlaceHolder/>
   </div>
-=======
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Button from '../comps/button'
-import FightButton from '../comps/battlebutton'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <div>
-        <Button/>
-      </div>
-      
-      <div>
-        <FightButton/>
-      </div>
-    </div>
->>>>>>> d7d4fc2a7c0280c211c95dcf0933598a9cddc32a
   )
 }
