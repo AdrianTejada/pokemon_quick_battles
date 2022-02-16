6fba6dfcad2e893b83c6da7a1992bc9396a9b2b5
AdrianTejada <atejada@my.bcit.ca>
2022-02-16 15:49:43 -0800

    pokemon card

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import CardPlaceHolder from '@/Comps/CardPlaceHolder'
 import Button from "@/Comps/Button";
 import FightButton from "@/Comps/FightButton";
-import Toggle from "@/Comps/Toggle";
-
+import Card from '@/Comps/Card'
 
 export default function Home() {
   const name = "ivysaur";
   return (<div>
     <img src={`https://img.pokemondb.net/sprites/black-white/normal/${name}.png`} alt="Ivysaur"/>
-    <CardPlaceHolder/>
+    {/* <CardPlaceHolder/>
     <Button/>
-    <FightButton/>
+    <FightButton/> */}
+    <Card/>
   </div>
   )
 }
