fda0c379ed93dee0b989b049e0f55b3127d72a9d
AdrianTejada <atejada@my.bcit.ca>
2022-02-18 22:59:37 -0800

    axios request working

--- Comps/SearchBar/index.js
+++ Comps/SearchBar/index.js
@@ -1,17 +1,79 @@
 import axios from "axios";
 import styled from "styled-components";
 import { useState } from "react";
+import { useProvider } from '@/utils/provider';
 
 const Cont = styled.div`
     min-width: 507px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-color: red;
 `
 
-export default function SearchBar () {
-    return <Cont>
+const SearchCont = styled.input`
+    min-width: 100%;
+    padding: 20px;
+    border-radius: 100px;
+    border-width: 0px;
+    font-size: 20px;
+`
+var timer = null;
 
+
+export default function SearchBar () {
+    const {
+        gen1,
+        gen2,
+        gen3,
+        gen4,
+        gen5,
+        gen6,
+        show_legendaries
+    } = useProvider()
+
+    const [data, setData] = useState([])
+
+
+    const Search = async (txt) => {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+
+        if (timer === null) {
+            timer = setTimeout(async()=>{
+            const result = await axios.get('/api/pokemon',{
+                params: {
+                    txt,
+                    gen1,
+                    gen2,
+                    gen3,
+                    gen4,
+                    gen5,
+                    gen6,
+                    show_legendaries
+                }})
+            setData(result.data);
+            timer = null;
+            console.log(data)
+            console.log({
+                txt,
+                gen1,
+                gen2,
+                gen3,
+                gen4,
+                gen5,
+                gen6,
+                show_legendaries
+            })
+            }, 1000)
+        }
+    }
+
+    return <Cont>
+        <SearchCont
+            placeholder="Search Pokemon"
+            onChange={(e)=>Search(e.target.value)}
+        />
     </Cont>
 }
--- pages/api/pokemon.js
+++ pages/api/pokemon.js
@@ -2,18 +2,32 @@ import { filter } from "@/utils/func"
 import pokemon from '@/utils/Pokemon.json'
 
 export default async function handler(req, res) {
-    const {txt, gen1, gen2, gen3, gen4, gen5, gen6, only_legendary} = req.query
+    const {txt, gen1, gen2, gen3, gen4, gen5, gen6, show_legendaries } = req.query
+    var list = []
 
-    const list = filter( pokemon, {
-        txt:txt,
-        gen1: gen1,
-        gen2: gen2,
-        gen3: gen3,
-        gen4: gen4,
-        gen5: gen5,
-        gen6: gen6,
-        only_legendary:only_legendary,
+    console.log({
+        "this is the text": txt,
+        gen1,
+        gen2,
+        gen3,
+        gen4,
+        gen5,
+        gen6,
+        show_legendaries
     })
+
+    // const list = filter( pokemon, {
+    //     txt:txt,
+    //     gen1: gen1,
+    //     gen2: gen2,
+    //     gen3: gen3,
+    //     gen4: gen4,
+    //     gen5: gen5,
+    //     gen6: gen6,
+    //     show_legendaries:show_legendaries,
+    // })
+
+
 
     res.status(200).json(list)
 }
