60fa26230482a0d1dcc751278e386329db2e7f03
AdrianTejada <atejada@my.bcit.ca>
2022-02-21 21:50:16 -0800

    minimum viable product complete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import { Background } from "@/Comps/Background"
 import SearchBar from "@/Comps/SearchBar"
 import styled from "styled-components"
+import logo from '@/public/assets/logo.png'
+import Image from "next/image"
+import Button from "@/Comps/Button"
+import { useRouter } from "next/router"
 
 const Main = styled.div`
     width: 100vw;
@@ -8,10 +12,34 @@ const Main = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+`
+
+const ButtonCont = styled.div`
+  margin-top: 30px;
+  height: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
 export default function Home() {
+  const router = useRouter()
+
   return <Main>
-  <Background/> 
+    <Background/> 
+  <Image
+    src={logo}
+    />
+    <ButtonCont>
+      <Button
+        text="Start!"
+        onClick={()=>router.push('/stadium')}
+      />
+      <Button
+        text="Settings"
+        onClick={()=>router.push('/settings')}
+      />
+    </ButtonCont>
 </Main>
 }
