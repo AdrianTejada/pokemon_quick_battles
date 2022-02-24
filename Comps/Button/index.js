2869f0f57b665ee204e69fe6e4d7c7dd305b7091
AdrianTejada <atejada@my.bcit.ca>
2022-02-23 22:56:16 -0800

    added button agian

---
+++ Comps/Button/index.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+import { useProvider } from "@/utils/provider";
+import { bgcolor } from '../variable'
+
+const MainButton = styled.button`
+    width:100px;
+    height:30px;
+    background:${props=>props.background};
+    border-radius:20px;
+    color:white;
+    box-shadow: 0px 1px 1px black;
+    border:none;
+`;
+
+const Button = ({
+    bg=bgcolor,
+    text="Start",
+    onClick=()=>{}
+}) => {
+
+    const{theme} = useProvider();
+
+    return <div onClick={()=>onClick()}>
+            <MainButton background={bg[theme]}>
+                {text}
+            </MainButton>
+    </div>
+}
+
+export default Button;
